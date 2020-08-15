79087b84e9dc211a1a5864525a5382aa092416ef
Human Fear <pnam5005@gmail.com>
2020-08-15 16:59:15 +0700

    Update MirrorNc.php

--- src/hachkingtohach1/MirrorNc/MirrorNc.php
+++ src/hachkingtohach1/MirrorNc/MirrorNc.php
@@ -30,7 +30,6 @@ class MirrorNc extends PluginBase {
 	CONST MAX_DISTANCE = 2;
 	CONST STANDARD_TIME = 100;
 	
-	/** @var $instance*/
 	private static $instance = null;
 	
 	/** @var int $maxMemory*/
